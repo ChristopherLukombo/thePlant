562359c261431e063c79a2d9c0d8300641cc5a27
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2019-06-25 17:46:07 +0200

    add PlantService

---
+++ ThePlant/Business/impl/PlantServiceImpl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ThePlant.Entity;
+
+
+
+namespace ThePlant.Business.impl
+{
+	public class PlantServiceImpl : PlantService
+	{
+		public List<PlantModel> findAll()
+		{
+			PlantModel plant1 = new PlantModel() { Name = "Plant numero 1", Price = 10 };
+			PlantModel plant2 = new PlantModel() { Name = "Plant numero 2", Price = 9.50 };
+
+			List<PlantModel> plants = new List<PlantModel>();
+			plants.Add(plant1);
+			plants.Add(plant2);
+
+			return plants;
+		}
+	}
+}
--- ThePlant/Controllers/PlantsController.cs
+++ ThePlant/Controllers/PlantsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ThePlant.Business;
+using ThePlant.Business.impl;
 using ThePlant.Entity;
 
 namespace ThePlant.Controllers
@@ -11,8 +13,15 @@ namespace ThePlant.Controllers
     [ApiController]
     public class PlantsController : ControllerBase
     {
-        // GET api/v1/health
-        [HttpGet]
+		private PlantService plantService;
+
+		public PlantsController()
+		{
+			this.plantService = new PlantServiceImpl();
+		}
+
+		// GET api/v1/health
+		[HttpGet]
         [Route("health")]
         public ActionResult<string> GetHealth()
         {
@@ -24,15 +33,8 @@ namespace ThePlant.Controllers
         [Route("plants")]
         public ActionResult<IEnumerable<PlantModel>> GetAllPlants()
         {
-            PlantModel plant1 = new PlantModel() { Name = "Plant numero 1", Price = 10 };
-            PlantModel plant2 = new PlantModel() { Name = "Plant numero 2", Price = 9.50 };
-
-            List<PlantModel> plants = new List<PlantModel>();
-            plants.Add(plant1);
-            plants.Add(plant2);
-
-            return plants;
-        }
+			return this.plantService.findAll();
+		}
 
         // GET api/values/5
         [HttpGet("{id}")]
